ec16afb05783b32a897d3148809eedef3f400879
Ben Ngahere <ngahereben@gmail.com>
2025-06-26 09:32:17 +1200

    added completion seed

--- server/db/seeds/fitness.js
+++ server/db/seeds/fitness.js
@@ -2,6 +2,7 @@ export async function seed(knex) {
   // Deletes ALL existing entries
   await knex('users').del()
   await knex('challenges').del()
+  await knex('completions').del()
 
   // Inserts seed entries
   await knex('users').insert([
@@ -56,4 +57,11 @@ export async function seed(knex) {
       difficulty: 'easy',
     },
   ])
+
+  await knex('completions').insert({
+    user_id: 'github|204113180',
+    challenge_id: 1,
+    completed_at: knex.fn.now(),
+    status: 'completed',
+  })
 }
