63e3f2168a7631adfdc1a17084a16db377166bdf
Seunghyun Min <markmin1316@gmail.com>
2025-06-26 15:30:56 +1200

    feat: create LoginButton.tsx

---
+++ client/components/LoginButton.tsx
@@ -0,0 +1,26 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
+
+export default function LoginButton() {
+  const { user, logout, loginWithRedirect } = useAuth0()
+
+  const handleSignOut = () => {
+    logout()
+  }
+
+  const handleSignIn = () => {
+    loginWithRedirect()
+  }
+
+  return (
+    <>
+      <IfAuthenticated>
+        <button onClick={handleSignOut}>Sign Out</button>
+        {user && <p>Welcome {user.name}</p>}
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <button onClick={handleSignIn}>Sign In</button>
+      </IfNotAuthenticated>
+    </>
+  )
+}
