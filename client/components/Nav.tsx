0e6d76263756871398df8db047aed62d76fb8e74
Callum Begley <cfbegley7@gmail.com>
2025-06-27 16:12:04 +1200

    profile done

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -3,10 +3,13 @@ import LoginButton from './LoginButton'
 
 export default function Nav() {
   return (
-    <div className="flex justify-around">
+    <div className="flex flex-wrap justify-around sm:text-xl">
       <Link to="/">Home</Link>
+      <p>-</p>
       <Link to="/profile">Profile</Link>
+      <p>-</p>
       <Link to="/log">Activity Log</Link>
+      <p>-</p>
       <LoginButton />
     </div>
   )
--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -21,23 +21,118 @@ function Profile() {
   // console.log('component all users', allUsers)
 
   return (
-    <div className="m-10 flex items-center justify-items-end rounded-2xl ring-2 ring-black ">
-      <img src="/warrior3.png" alt="character" className="h-40 w-40" />
-      <div className="grid grid-cols-2">
-        <p>Name: </p>
-        <p>{user?.name}</p>
-        <p>Class: </p>
-        <p>{user?.class}</p>
-        <p>Rank: </p>
-        <p>{user?.rank}</p>
-        <p>Level: </p>
-        <p>{user?.level}</p>
-        <p>XP:</p> <p>{user?.xp}</p>
-        <p>Strength:</p> <p>{user?.str}</p>
-        <p>Dexterity:</p> <p>{user?.dex}</p>
-        <p>Intelligence:</p> <p>{user?.int}</p>
+    <>
+      <div className="mx-auto max-w-2xl rounded-lg border border-gray-700 bg-gray-800 p-6 py-8 font-mono text-green-300 shadow-xl">
+        <h3 className="mb-4 border-b-2 border-green-700 pb-4 text-center text-2xl font-bold text-green-400">
+          {' '}
+          Profile
+        </h3>
+        <div className="justify-content-center mt-10 flex flex-wrap items-center gap-1.5 justify-self-center rounded-2xl p-4 ring-2 ring-gray-300 sm:gap-4">
+          <img
+            src="/warrior.webp"
+            alt="character"
+            className="mx-auto h-auto w-40"
+          />
+          <div className="mx-auto grid grid-cols-[1fr_2fr] gap-1 font-semibold">
+            <p>Name: </p>
+            <p className="text-center capitalize text-white">{user?.name}</p>
+            <p>Class: </p>
+            <p className="text-center capitalize text-white">{user?.class}</p>
+            <p>Rank: </p>
+            <p className="text-center capitalize text-white">{user?.rank}</p>
+            <p>Level: </p>
+            <p className="text-center text-white">{user?.level}</p>
+            <p>XP:</p>{' '}
+            <div className="relative max-h-4">
+              <p className="absolute z-20 m-0 ml-10 text-white">{user?.xp}</p>
+              <div
+                className={`relative z-0 h-4 w-full translate-y-1 bg-gray-700`}
+              >
+                <div
+                  style={{ width: `${user?.xp}%` }}
+                  // work out based on next level xp
+                  className={`z-10 h-4 bg-green-700`}
+                ></div>
+              </div>
+            </div>
+            <p>Str:</p>{' '}
+            <div className="relative max-h-4">
+              <p className="absolute z-20 m-0 ml-10 text-white">{user?.str}</p>
+              <div
+                className={`relative z-0 h-4 w-full translate-y-1 bg-gray-700`}
+              >
+                <div
+                  style={{ width: `${user?.str}%` }}
+                  className={`z-10  h-4 bg-green-700`}
+                ></div>
+              </div>
+            </div>
+            <p>Dex:</p>{' '}
+            <div className="relative max-h-4">
+              <p className="absolute z-20 m-0 ml-10 text-white">{user?.dex}</p>
+              <div
+                className={`relative z-0 h-4 w-full translate-y-1 bg-gray-700`}
+              >
+                <div
+                  style={{ width: `${user?.dex}%` }}
+                  className={`z-10 h-4 bg-green-700`}
+                ></div>
+              </div>
+            </div>
+            <p>Int:</p>{' '}
+            <div className="relative max-h-4">
+              <p className="absolute z-20 m-0 ml-10 text-white">{user?.int}</p>
+              <div
+                className={`relative z-0 h-4 w-full translate-y-1 bg-gray-700`}
+              >
+                <div
+                  style={{ width: `${user?.int}%` }}
+                  className={`z-10 h-4 bg-green-700`}
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+
+      <div className="mx-auto mt-8 max-w-2xl rounded-lg border border-gray-700 bg-gray-800 p-6 py-8 font-mono text-green-300 shadow-xl">
+        <h3 className="mb-4 border-b-2 border-green-700 pb-4 text-center text-2xl font-bold text-green-400">
+          {' '}
+          Achievements
+        </h3>
+        {/* <div className="mt-10 flex flex-wrap items-center justify-start gap-8 justify-self-center rounded-2xl p-4 ring-2 ring-gray-300"> */}
+        <div className="mt-10 grid grid-cols-3 items-center justify-start gap-8 justify-self-center rounded-2xl p-4 ring-2 ring-gray-300 sm:grid-cols-6">
+          <p className="text-sm">
+            <img
+              src="/achievement.webp"
+              alt="character"
+              className="h-auto w-20"
+            />
+            First time for everything
+          </p>
+          <img
+            src="/achievement.webp"
+            alt="character"
+            className="h-auto w-20"
+          />
+          <img
+            src="/achievement.webp"
+            alt="character"
+            className="h-auto w-20"
+          />
+          <img
+            src="/achievement.webp"
+            alt="character"
+            className="h-auto w-20"
+          />
+          <img
+            src="/achievement.webp"
+            alt="character"
+            className="h-auto w-20"
+          />
+        </div>
+      </div>
+    </>
   )
 }
 
