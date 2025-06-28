13a542504d9fabb0927b902c82375f3ebe784dea
Ben Ngahere <ngahereben@gmail.com>
2025-06-28 13:43:25 +1200

    db/users auth0 configured

--- server/db/users.ts
+++ server/db/users.ts
@@ -11,13 +11,60 @@ export async function getUserById(id: number) {
   return user as User
 }
 
+export async function getUserByAuthId(
+  authId: string,
+): Promise<User | undefined> {
+  const user = await db('users')
+    .select(
+      'id',
+      'auth_id',
+      'name',
+      'xp',
+      'level',
+      'rank',
+      'str',
+      'dex',
+      'int',
+      'missed',
+      'class',
+      'appearance',
+    )
+    .where('auth_id', authId)
+    .first()
+  return user as User | undefined
+}
+
 export async function addUser(data: UserData) {
-  const [id] = await db('users').insert(data)
+  const [id] = await db('users').insert({
+    auth_id: data.auth_id,
+    name: data.name,
+    class: data.class,
+
+    xp: 0,
+    level: 1,
+    rank: 'bronze',
+    str: 0,
+    dex: 0,
+    int: 0,
+    missed: 0,
+    appearance: 0,
+  })
   return id
 }
 
 export async function updateUser(data: UpdateUser) {
-  if (!data.id) return
+  if (!data.id) {
+    console.error('UpdateUser fucntion error')
+    return 0
+  }
   const res = await db('users').where('id', data.id).update(data)
   return res
+}
+
+export async function updateUserByAuthId(
+  authId: string,
+  updates: Partial<Omit<User, 'id' | 'auth_id'>>, // Stops 'id' & 'auth_id' from updating
+): Promise<number> {
+  const updatedRows = await db('users').where('auth_id', authId).update(updates)
+  return updatedRows
 }
