37574ca7ebea2c1fd8e1adc90ce288238d3370a1
Ben Ngahere <ngahereben@gmail.com>
2025-06-30 11:10:17 +1200

    routes/challenges.ts auth0 configured

--- client/hooks/useChallenges.ts
+++ client/hooks/useChallenges.ts
@@ -3,7 +3,7 @@ import { getChallenges, getSingleChallenge } from '../apis/challenges'
 import { useAuth0 } from '@auth0/auth0-react'
 
 export function useChallenges() {
-  const { getAccessTokenSilently } = useAuth0()
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0()
 
   return useQuery({
     queryKey: ['challenges'],
--- server/routes/challenges.ts
+++ server/routes/challenges.ts
@@ -1,33 +1,34 @@
 import { Router } from 'express'
 import * as db from '../db/challenges'
 import { getChallengesDoneToday } from '../db/completionsDb'
-// import checkJwt from '../auth0'
+import checkJwt from '../auth0'
 
 const router = Router()
 
-router.get(
-  '/',
-  /*checkJwt*/ async (req, res) => {
-    try {
-      // const userId = req.auth?.sub
-      const userId = 2
+router.get('/', checkJwt, async (req, res) => {
+  try {
+    const userId = req.auth?.sub
 
-      if (!userId) return res.sendStatus(401)
-
-      const allChallenges = await db.getAllChallenges()
-      const completedChallenges = await getChallengesDoneToday(Number(userId))
-
-      const uncompletedChallenges = allChallenges.filter(
-        (challenge) => !completedChallenges.includes(challenge.id),
-      )
-
-      res.json(uncompletedChallenges)
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({ message: 'Something went wrong!' })
+    if (!userId) {
+      console.warn('No userId to get Challenges')
+      return res.sendStatus(401)
     }
-  },
-)
+
+    const completedChallenges = await getChallengesDoneToday(userId)
+    const allChallenges = await db.getAllChallenges()
+
+    const completedUserId = new Set(completedChallenges)
+
+    const uncompletedChallenges = allChallenges.filter(
+      (challenge) => !completedUserId.has(challenge.id), // filters completed challenges based on userId
+    )
+
+    res.json(uncompletedChallenges)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong!' })
+  }
+})
 
 router.get('/:id', async (req, res) => {
   try {
