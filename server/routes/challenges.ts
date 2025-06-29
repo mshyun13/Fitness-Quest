d44d399f4002205129bb3504f8ce89169482af62
Ben Ngahere <ngahereben@gmail.com>
2025-06-30 11:28:32 +1200

    routes/challenges.ts auth0 configured

--- server/routes/challenges.ts
+++ server/routes/challenges.ts
@@ -1,27 +1,43 @@
 import { Router } from 'express'
-import * as db from '../db/challenges'
-import { getChallengesDoneToday } from '../db/completionsDb'
+import * as challengesDb from '../db/challenges'
+import * as completionsDb from '../db/completionsDb'
+import * as usersDb from '../db/users'
 import checkJwt from '../auth0'
 
 const router = Router()
 
 router.get('/', checkJwt, async (req, res) => {
   try {
-    const userId = req.auth?.sub
+    const auth0Id = req.auth?.sub
 
-    if (!userId) {
+    if (!auth0Id) {
       console.warn('No userId to get Challenges')
       return res.sendStatus(401)
     }
 
-    const completedChallenges = await getChallengesDoneToday(userId)
-    const allChallenges = await db.getAllChallenges()
+    const dbUser = await usersDb.getUserByAuthId(auth0Id)
+
+    if (!dbUser) {
+      console.warn('User not found in DB', auth0Id)
+      return res.json([])
+    }
+
+    const userId = dbUser.id
+    // console.log()
+
+    const allChallenges = await challengesDb.getAllChallenges()
+    // console.log()
+
+    const completedChallenges =
+      await completionsDb.getChallengesDoneToday(userId)
+    // console.log()
 
     const completedUserId = new Set(completedChallenges)
 
     const uncompletedChallenges = allChallenges.filter(
       (challenge) => !completedUserId.has(challenge.id), // filters completed challenges based on userId
     )
+    // console.log()
 
     res.json(uncompletedChallenges)
   } catch (error) {
@@ -33,7 +49,7 @@ router.get('/', checkJwt, async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const id = Number(req.params.id)
-    const challenge = await db.getSingleChallenge(id)
+    const challenge = await challengesDb.getSingleChallenge(id)
     res.json(challenge)
   } catch (error) {
     console.log(error)
