fa5257e69221da340650830e1803042affb03c2f
Ben Ngahere <ngahereben@gmail.com>
2025-07-01 11:58:38 +1200

    sidequest XP addition

--- server/db/sidequests.ts
+++ server/db/sidequests.ts
@@ -43,5 +43,44 @@ export async function addSideQuestXp(data: SideQuestData) {
       attribute,
       completed_at,
     })
+
+    // Get users current xp and attribute stats
+    const user = await trx('users')
+      .where('id', user_id)
+      .select('xp', 'level', 'str', 'dex', 'int')
+      .first()
+
+    if (!user) {
+      throw new Error('User Not Found')
+    }
+
+    const updatedUser: Partial<User> = { ...user } as User
+
+    // Add SideQuest XP
+    updatedUser.xp = (updatedUser.xp || 0) + SIDE_QUEST_XP
+
+    // Recalculate level based on total XP
+    const newCalculatedLevel = getLevelFromTotalXp(updatedUser.xp)
+
+    // Set XP relative to new level
+    updatedUser.level = newCalculatedLevel
+    const xpAtStartOfNewLevel = getXpForLeveling(updatedUser.level)
+    updatedUser.xp = updatedUser.xp - xpAtStartOfNewLevel
+
+    // Which attribute to update? And apply update
+    const attributeToUpdate = attribute as 'str' | 'dex' | 'int'
+
+    const currentAttributeValue = user[attributeToUpdate]
+    let newAttributeValue = currentAttributeValue + 1
+    if (newAttributeValue > 100) {
+      const bonusXpFromAttribute = newAttributeValue - 100
+      newAttributeValue = 1
+
+      updatedUser.xp = (updatedUser.xp || 0) + bonusXpFromAttribute
+
+      const levelAfterBonus = getLevelFromTotalXp(
+        getXpForLeveling(updatedUser.level) + updatedUser.xp,
+      )
+    }
   })
 }
