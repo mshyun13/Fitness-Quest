c2a12e67445b33704ee4ebc215eb913e100136aa
Callum Begley <cfbegley7@gmail.com>
2025-07-02 20:29:15 +1200

    ctrl s

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -37,10 +37,18 @@ export default {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: '/app/storage/prod.sqlite3',
+      //filename: '/app/storage/prod.sqlite3',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 5432,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      ssl: { rejectUnauthorized: false },
     },
     pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+      //afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+      min: 2,
+      max: 10,
     },
   },
 }
